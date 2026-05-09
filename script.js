3e8e7b2991d2ab4511c592b6a367b99dd67cc11c
jlgibson1203-lang <jlgibson1203@gmail.com>
2026-05-09 08:49:54 -0700

    Remove scrollToSection function from script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth' });
-    }
-}
+
